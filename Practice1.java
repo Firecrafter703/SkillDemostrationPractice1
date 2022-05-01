5c6e05c7db86d2c42067348d1ee209e53592c53a
DK703 <91508871+Firecrafter703@users.noreply.github.com>
2022-05-01 12:04:13 -0700

    Here we go

---
+++ Practice1.java
@@ -0,0 +1,9 @@
+public class Practice1{
+
+public static int subtract(int m, int n)
+{
+    //m-n
+return m;
+}
+
+}
---
+++ Practice1Tests.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class Practice1Tests
+{
+@Test
+public void subtraction()
+{
+    assertEquals(2, Practice1.subtract(3,1));
+
+
+}
+}
